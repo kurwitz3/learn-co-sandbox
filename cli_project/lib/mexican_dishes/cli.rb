63316e27d7bc71c2b6d6b2eefe3e5b9f3d686c6d
Richard Kurwitz <kurwitz3@aol.com>
2020-09-10 02:29:00 +0000

    save

--- cli_project/lib/mexican_dishes/cli.rb
+++ cli_project/lib/mexican_dishes/cli.rb
@@ -2,8 +2,8 @@
 class MexicanDishes::Cli 
   def  starter 
     puts "Welcome to Taste of Mexico! Where you can find the recipes to your 
-          favorite authentic mexican food! If you would like to see a list 
-          of dishes press y!"
+favorite authentic Mexican food! 
+          If you would like to see a list of dishes press y!"
         first_user_input 
         second_user_input 
     end
@@ -15,14 +15,14 @@ class MexicanDishes::Cli
   def get_user_dish 
     puts "Choose your favorite dish by number to get the link to the recipe!"
       @new =[1,2,3,4,5,6,6]
-        @dish.each.with_index(1) do |dish,number|
+        @new.each.with_index(1) do |dish,number|
       puts "#{number}. #{dish}"
     end 
   end
    
    def first_user_input 
      answer = gets.strip
-      if answer == 'y'
+      if answer == 'y' || 'Y'
        get_user_dish
       else 
        starter 
@@ -31,10 +31,10 @@ class MexicanDishes::Cli
   
   
    def second_user_input 
-     answer = gets.strip
-   if answer.to_s <= @new.length && answer.to_s > 0 
-     binding.pry
-     @new[answer.to_s]
+     answer = gets.strip.to_i
+   if answer <= @new.length && answer > 0 
+     @new[answer - 1 ]
+   else 
    end 
  end 
      
